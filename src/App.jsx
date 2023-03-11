6be5d0fa4794996fba50da6f71c4dd2a8f7788b2
jwseph <kodomo.oishii@gmail.com>
2023-03-11 00:34:17 -0800

    Font fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,80 +2,93 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import { Route, Link, Routes, useNavigate } from 'react-router-dom';
 
-const NewParty = ({ name, size, contact, onNameChange, onSizeChange, onContactChange, onSubmit }) => (
-  <div className="w-full max-w-md space-y-8 mb-8">
-    <div>
-      <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">New party</h2>
-      <p className="mt-2 text-center text-sm text-gray-600">
-        Enter your name, party size, and contact information
-      </p>
-    </div>
-    <form className="mt-8 space-y-6">
-      <input type="hidden" name="remember" value="true"/>
-      <div className="-space-y-px rounded-md shadow-sm">
-        <div>
-          <label htmlFor="name" className="sr-only">Name</label>
-          <input defaultValue={name} onChange={e => onNameChange(e.target.value.trim())} id="name" name="name" type="name" autoComplete="off" className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" placeholder="Name"/>
-        </div>
-        <div>
-          <label htmlFor="size" className="sr-only">Party size</label>
-          <input defaultValue={size} onChange={e => onSizeChange(e.target.value.trim())} id="size" name="size" type="size" autoComplete="off" className="relative block w-full border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" placeholder="Party size"/>
-        </div>
-        <div>
-          <label htmlFor="contact" className="sr-only">Phone/email</label>
-          <input defaultValue={contact} onChange={e => onContactChange(e.target.value.trim())} id="contact" name="contact" type="contact" autoComplete="off" className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" placeholder="Phone/email"/>
-        </div>
-      </div>
+function NewParty({ name, size, contact, onNameChange, onSizeChange, onContactChange, onSubmit }) {
+  useEffect(() => {
+    document.title = 'Waitlist Demo';
+  }, []);
+  return (
+    <div className="w-full max-w-md space-y-8 mb-8">
       <div>
-        <button type='button' onClick={onSubmit} className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-          Submit
-        </button>
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">New party</h2>
+        <p className="mt-2 text-center text-sm text-gray-600">
+          Enter your name, party size, and contact information
+        </p>
       </div>
-    </form>
-  </div>
-)
-
-const Partywaiting = ({ name, size, contact, timestamp }) => (
-  <div className="w-full max-w-md space-y-8 mb-8">
-    <div>
-      <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Request sent</h2>
-      <p className="mt-2 text-center text-sm text-gray-600">
-        You will be notified when your table is ready
-      </p>
-    </div>
-    <form className="mt-8 space-y-6">
-      <input type="hidden" name="remember" value="true"/>
-      <div className="-space-y-px rounded-md shadow-sm">
-        <div>
-          <label htmlFor="name" className="sr-only">Name</label>
-          <input value={name} id="name" name="name" type="name" autoComplete="off" className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" placeholder="Name" disabled/>
+      <form className="mt-8 space-y-6">
+        <input type="hidden" name="remember" value="true"/>
+        <div className="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label htmlFor="name" className="sr-only">Name</label>
+            <input defaultValue={name} onChange={e => onNameChange(e.target.value.trim())} id="name" name="name" type="name" autoComplete="off" className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" placeholder="Name"/>
+          </div>
+          <div>
+            <label htmlFor="size" className="sr-only">Party size</label>
+            <input defaultValue={size} onChange={e => onSizeChange(e.target.value.trim())} id="size" name="size" type="size" autoComplete="off" className="relative block w-full border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" placeholder="Party size"/>
+          </div>
+          <div>
+            <label htmlFor="contact" className="sr-only">Phone/email</label>
+            <input defaultValue={contact} onChange={e => onContactChange(e.target.value.trim())} id="contact" name="contact" type="contact" autoComplete="off" className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" placeholder="Phone/email"/>
+          </div>
         </div>
         <div>
-          <label htmlFor="size" className="sr-only">Party size</label>
-          <input value={size} id="size" name="size" type="size" autoComplete="off" className="relative block w-full border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" placeholder="Party size" disabled/>
-        </div>
-        <div>
-          <label htmlFor="contact" className="sr-only">Phone/email</label>
-          <input value={contact} id="contact" name="contact" type="contact" autoComplete="off" className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" placeholder="Phone/email" disabled/>
-        </div>
-      </div>
-      <div>
-        <Link to='/'>
-          <button type="button" className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            onClick={async () => {
-              let url = `https://kamiak-io.fly.dev/waitlist/remove?timestamp=${encodeURIComponent(timestamp)}`;
-              await fetch(url, {method: 'POST'});
-            }}
-          >
-            Cancel
+          <button type='button' onClick={onSubmit} className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Submit
           </button>
-        </Link>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+function PartyWaiting({ name, size, contact, timestamp }) {
+  useEffect(() => {
+    document.title = 'Waitlist Demo - Waiting';
+  }, []);
+  return (
+    <div className="w-full max-w-md space-y-8 mb-8">
+      <div>
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Request sent</h2>
+        <p className="mt-2 text-center text-sm text-gray-600">
+          You will be notified when your table is ready
+        </p>
       </div>
-    </form>
-  </div>
-)
+      <form className="mt-8 space-y-6">
+        <input type="hidden" name="remember" value="true"/>
+        <div className="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label htmlFor="name" className="sr-only">Name</label>
+            <input value={name} id="name" name="name" type="name" autoComplete="off" className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" placeholder="Name" disabled/>
+          </div>
+          <div>
+            <label htmlFor="size" className="sr-only">Party size</label>
+            <input value={size} id="size" name="size" type="size" autoComplete="off" className="relative block w-full border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" placeholder="Party size" disabled/>
+          </div>
+          <div>
+            <label htmlFor="contact" className="sr-only">Phone/email</label>
+            <input value={contact} id="contact" name="contact" type="contact" autoComplete="off" className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" placeholder="Phone/email" disabled/>
+          </div>
+        </div>
+        <div>
+          <Link to='/'>
+            <button type="button" className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={async () => {
+                let url = `https://kamiak-io.fly.dev/waitlist/remove?timestamp=${encodeURIComponent(timestamp)}`;
+                await fetch(url, {method: 'POST'});
+              }}
+            >
+              Cancel
+            </button>
+          </Link>
+        </div>
+      </form>
+    </div>
+  )
+}
 
 function Waitlist() {
+  useEffect(() => {
+    document.title = 'Waitlist Demo - Waitlist';
+  }, []);
   return (
     <div className="w-full max-w-md space-y-8 mb-8">
       <div>
@@ -127,7 +140,7 @@ function App() {
             }}
           />
         }></Route>
-          <Route path='/waiting' element={<Partywaiting name={name} size={size} contact={contact} timestamp={timestamp}/>}></Route>
+          <Route path='/waiting' element={<PartyWaiting name={name} size={size} contact={contact} timestamp={timestamp}/>}></Route>
           <Route path='/waitlist' element={<Waitlist/>}></Route>
       </Routes>
       <footer className='fixed bottom-2 p-4 text-sm text-gray-800'>
